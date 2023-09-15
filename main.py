cfbc73db5f049b65bc315fb3581943a4c54241a0
wintercresss <135405194+wintercresss@users.noreply.github.com>
2023-09-15 01:28:09 -0500

    Super basic get implementation, just making sure server works

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+
+class Song(Resource):
+    def get(self):
+        return {"song": "welcome to new york"}
+    
+
+
+
+api.add_resource(Song, "/song")
+
+if __name__ == "__main__":
+    app.run(debug=True)
